9be7de134ca24ea387f4c4aee542deb32cf3b032
Chase <chaseelliottpatterson@gmail.com>
2024-11-09 12:19:07 -0800

    working detection with gpu

--- scripts/main.py
+++ scripts/main.py
@@ -1,28 +1,44 @@
 from ultralytics import YOLO
- 
-# Load the model.
-model = YOLO('models/yolo11n-obb.pt')
- 
-# # Training.
-# results = model.train(
-#    data='data/dice-reader_test_set_cov/data.yaml',
-#    imgsz=640,
-#    epochs=50,
-#    batch=8,
-#    name='yolov8n_v8_obb_50e'
-# )
+import datetime
 
-results = model.train(
-   data='data/dice-reader_test_set_cov/data.yaml',
-    epochs=300,                 # Increase if needed
-    batch=16,                   # Adjust based on GPU capacity
-    imgsz=640,                  # Resolution of input images
-    lr0=0.002,                  # Initial learning rate
-    optimizer='AdamW',          # Optimizer
-    weight_decay=0.0005,        # Regularization to prevent overfitting
-    augment=True,                # Enable data augmentation
-    patience = 30,
-    device=0,
-    name='yolov8n_v8_obb_50e'
-)
-print("done")
+def main():
+   timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")
+   model = YOLO('models/yolo11n-obb.pt')
+   
+   # # Training debugging
+   # results = model.train(
+   #    data='../data/dice-reader_test_set_cov/data.yaml',
+   #    imgsz=640,
+   #    epochs=50,
+   #    batch=8,
+   #    # amp=False,
+   #    # device='cpu',
+   #    name='yolov8n_v8_obb_50e'
+
+   # )
+
+   results = model.train(
+      data='C:/Users/Studio/Documents/GitHub/dice-reader/data/dice-reader-139_cov/data.yaml',
+      epochs=300,                 # Increase if needed
+      batch=16,                   # Adjust based on GPU capacity
+      imgsz=1024,                  # Resolution of input images
+      lr0=0.002,                  # Initial learning rate
+      optimizer='AdamW',          # Optimizer
+      weight_decay=0.0005,        # Regularization to prevent overfitting
+      patience = 30,
+      device=0,
+      time=1,
+      name=f'yolov8n_obb_{timestamp}'
+   )
+   # results = model.train(
+   #    data='C:/Users/Studio/Documents/GitHub/dice-reader/data/dice-reader-139_cov/data.yaml',
+   #    # data='../data/dice-dice-reader-139_cov/data.yaml',
+   #    epochs=300,                 # Increase if needed
+   #    batch=32,                   # Adjust based on GPU capacity
+   #    device=0,
+   #    name=f'yolov8n_obb_{timestamp}'
+   # )
+
+   print("done")
+if __name__ == "__main__":
+    main()
--- scripts/test_results.py
+++ scripts/test_results.py
@@ -3,14 +3,14 @@ import cv2
 import matplotlib.pyplot as plt
 
 # Load the trained YOLOv8 model
-model = YOLO('C:/Users/Studio/Documents/GitHub/dice-reader/runs/obb/yolov8n_v8_obb_50e4/weights/best.pt')
+model = YOLO('C:/Users/Studio/Documents/GitHub/dice-reader/scripts/runs/obb/yolov8n_obb_2024-11-09_12-03/weights/best.pt')
 
 # Load an image
-image_path = "C:/Users/Studio/Documents/GitHub/dice-reader/data/dice-reader_test_set_cov/images/train/DJI_20241005234616_0252_D.JPG"  # Replace with the path to your image
+image_path = "C:/Users/Studio/Documents/GitHub/dice-reader/data/dice-reader-139_cov/images/val/DJI_20241005232542_0139_D.JPG"  # Replace with the path to your image
 image = cv2.imread(image_path)
 
 # Run inference
-results = model(image)
+results = model(image, conf=0.1)
 
 # Print detection results
 print(results)  # This will print bounding box coordinates, classes, and confidence scores
