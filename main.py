0fe4070c4544c4bfa037b1b7b07b9959aba94467
Chase <chaseelliottpatterson@gmail.com>
2024-11-08 22:30:31 -0800

    Update main.py

--- main.py
+++ main.py
@@ -14,13 +14,14 @@ model = YOLO('models/yolo11n-obb.pt')
 
 results = model.train(
    data='data/dice-reader_test_set_cov/data.yaml',
-    epochs=100,                 # Increase if needed
+    epochs=300,                 # Increase if needed
     batch=16,                   # Adjust based on GPU capacity
     imgsz=640,                  # Resolution of input images
     lr0=0.002,                  # Initial learning rate
     optimizer='AdamW',          # Optimizer
     weight_decay=0.0005,        # Regularization to prevent overfitting
     augment=True,                # Enable data augmentation
+    patience = 30,
     device=0,
     name='yolov8n_v8_obb_50e'
 )
