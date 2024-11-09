d34f1a499226125b41721570766f4fc6c59d8701
Chase <chaseelliottpatterson@gmail.com>
2024-11-09 13:37:46 -0800

    cleaning

--- scripts/main_model_training.py
+++ scripts/main_model_training.py
@@ -1,23 +1,14 @@
 from ultralytics import YOLO
+import os
 import datetime
 
 def main():
-   timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")
-   model = YOLO('models/yolo11n-obb.pt')
-   
-   # # Training debugging
-   # results = model.train(
-   #    data='../data/dice-reader_test_set_cov/data.yaml',
-   #    imgsz=640,
-   #    epochs=50,
-   #    batch=8,
-   #    # amp=False,
-   #    # device='cpu',
-   #    name='yolov8n_v8_obb_50e'
+   model = YOLO('C:/Users/Studio/Documents/GitHub/dice-reader/scripts/models/yolo11n-obb.pt')
+   save_dir = 'C:/Users/Studio/Documents/GitHub/dice-reader'
+   os.makedirs(save_dir, exist_ok=True)
+   timestamp = datetime.datetime.now().strftime("%Y_%m_%d__%H_%M")
 
-   # )
-
-   results = model.train(
+   model.train(
       data='C:/Users/Studio/Documents/GitHub/dice-reader/data/dice-reader-139_cov/data.yaml',
       epochs=300,                 # Increase if needed
       batch=16,                   # Adjust based on GPU capacity
@@ -30,15 +21,6 @@ def main():
       time=1,
       name=f'yolov8n_obb_{timestamp}'
    )
-   # results = model.train(
-   #    data='C:/Users/Studio/Documents/GitHub/dice-reader/data/dice-reader-139_cov/data.yaml',
-   #    # data='../data/dice-dice-reader-139_cov/data.yaml',
-   #    epochs=300,                 # Increase if needed
-   #    batch=32,                   # Adjust based on GPU capacity
-   #    device=0,
-   #    name=f'yolov8n_obb_{timestamp}'
-   # )
 
-   print("done")
 if __name__ == "__main__":
     main()
